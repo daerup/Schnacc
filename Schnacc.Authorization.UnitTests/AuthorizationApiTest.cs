d6df961b716918e0c15824980fd8b3e4974cbff1
Dario Portmann <email@portmann-dar.io>
2019-12-19 08:11:31 +0100

    Added Authorization project

---
+++ Schnacc.Authorization.UnitTests/AuthorizationApiTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Schnacc.Authorization.UnitTests
+{
+    using Xunit;
+
+    public class AuthorizationApiTest
+    {
+        private AuthorizationApi testee = new AuthorizationApi();
+
+        [Fact]
+        private void userCanRegister()
+        {
+            //this.testee.RegisterWithEmail("liam1.gloggner@gmail.com", "123456789", "homoGae");
+        }
+    }
+}
---
+++ Schnacc.Authorization/AuthorizationApi.cs
@@ -0,0 +1,32 @@
+﻿namespace Schnacc.Authorization
+{
+    using System.Threading.Tasks;
+
+    using Firebase.Auth;
+
+    public class AuthorizationApi
+    {
+        private readonly FirebaseAuthProvider authProvider;
+
+        public AuthorizationApi()
+        {
+            this.authProvider = new FirebaseAuthProvider(new FirebaseConfig(AuthConfig.ApiKey));
+        }
+
+        public void RegisterWithEmail(string email, string password, string displayName)
+        {
+            FirebaseAuthLink userWithEmailAndPasswordAsync = this.authProvider.CreateUserWithEmailAndPasswordAsync(email, password, displayName).Result;
+            this.authProvider.SendEmailVerificationAsync(userWithEmailAndPasswordAsync);
+        }
+
+        public string SignInWithEmail(string email, string password)
+        {
+            return this.authProvider.SignInWithEmailAndPasswordAsync(email, password).Result.FirebaseToken;
+        }
+
+        public string SignInAnonymously()
+        {
+            return this.authProvider.SignInAnonymouslyAsync().Result.FirebaseToken;
+        }
+    }
+}
